f212042670a4156c4df581cfbcd59baf63d763fc
pavlovw <133174113+pavlovw@users.noreply.github.com>
2026-09-22 09:23:54 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 // === NUEVA CONFIGURACIÓN DE SEGURIDAD (CORS) ===
 const origenesPermitidos = [
   "http://localhost:3000",
-  "https://agro-vox-demo.vercel.app/" // <-- REEMPLAZA ESTO CON TU URL DE VERCEL (sin / al final)
+  "https://agro-vox-demo.vercel.app" 
 ];
 
 // Configuración para WebSockets (Socket.io)
